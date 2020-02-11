d193d60ec7cca53e65c602cac68bba434bde8606
Jakob Schreiner <jakob2602@gmail.com>
2020-02-11 14:55:48 +0100

    user VectorInt to pass array to pybind

--- experiments/ConcentricCircles/coupled_odesolver.py
+++ experiments/ConcentricCircles/coupled_odesolver.py
@@ -61,7 +61,7 @@ class CoupledODESolver:
 
         self.ode_solver = self.ode_module.LatticeODESolver(
             self._function_space_VS._cpp_object,
-            cell_function.array(),
+            VectorInt(cell_function.array()),
             self._parameters.parameter_map
         )
 
